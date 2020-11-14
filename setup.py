d5a41f37901862afdd82ad599c1b752a342a680e
AyrtonB <AyrtonBourn@Outlook.com>
2020-11-14 14:16:10 +0000

    updated setup and gitignore

--- setup.py
+++ setup.py
@@ -1,20 +1,20 @@
 import setuptools
 
-with open("satip/README.md", "r") as fh:
+with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="satip",
-    version="0.0.5",
+    name="energynews",
+    version="0.0.1",
     author="Ayrton Bourn",
     author_email="ayrton@futureenergy.associates",
-    description="Satip is a Python library for satellite image processing",
+    description="EnergyNews is a Python library for retrieving the latest energy journalism",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/Future-Energy-Associates/satellite_image_processing", # change after merging back to ocf
+    url="https://github.com/AyrtonB/Energy-News",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
     ],
-    python_requires=">=3.6",
+    python_requires=">=3.7",
 )
