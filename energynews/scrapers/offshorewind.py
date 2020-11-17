44d144cf5e10d3c46790285e1487ce2d87933c0b
AyrtonB <AyrtonBourn@Outlook.com>
2020-11-17 10:51:02 +0000

    higher quality images and cleaning lead for offshorewind

--- energynews/scrapers/offshorewind.py
+++ energynews/scrapers/offshorewind.py
@@ -19,11 +19,12 @@ def retrieve_all_current_articles():
     articles = list(pd
                     .DataFrame(xml['rss']['channel']['item'])
                     .pipe(lambda df: df.assign(category=df['category'].str.join(', ')))
+                    .pipe(lambda df: df.assign(description=df['description'].str.replace(' [&#8230;]', '')))
                     .pipe(lambda df: df.assign(pubDate=pd.to_datetime(df['pubDate']).dt.strftime('%Y-%m-%d %H:%M')))
                     .rename(columns={
                         'link': 'article_url',
                         'description': 'lead',
-                        'image': 'image_url',
+                        'image_medium': 'image_url',
                         'pubDate': 'date',
                     })
                     .T
